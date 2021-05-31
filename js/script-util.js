d9ad367e91d469afcadae2df4e940e4d72629b40
LisetALV <adrilis1213@gmail.com>
2021-05-31 04:01:49 -0500

    sitio estatico

--- js/script-util.js
+++
@@ -1,5 +0,0 @@
-function logAccess(name){
-    var hoy = new Date();
-    console.log('Acceso Registrado a ' + name);
-    console.log(hoy.getHours()+ ":" + hoy.getMinutes() + ":" + hoy.getSeconds());
-}
